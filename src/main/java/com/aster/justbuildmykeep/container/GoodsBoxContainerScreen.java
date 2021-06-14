61ab7bef42e2f68fd4badabfa26e3b46edbe367d
guodh1 <guodh1@yonyou.com>
2021-06-14 09:35:56 +0800

    木制盒子

--- src/main/java/com/aster/justbuildmykeep/container/GoodsBoxContainerScreen.java
+++ src/main/java/com/aster/justbuildmykeep/container/GoodsBoxContainerScreen.java
@@ -6,12 +6,14 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.SlabType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 public class GoodsBoxContainerScreen extends ContainerScreen<GoodsBaxContainer> {
-    private final ResourceLocation OBSIDIAN_CONTAINER_RESOURCE_A = new ResourceLocation(Utils.MOD_ID, "textures/gui/container/goodBox_dispenser_a.png");
-    private final ResourceLocation OBSIDIAN_CONTAINER_RESOURCE_B = new ResourceLocation(Utils.MOD_ID, "textures/gui/container/goodBox_dispenser_b.png");
+    private final ResourceLocation OBSIDIAN_CONTAINER_RESOURCE_A = new ResourceLocation(Utils.MOD_ID, "textures/gui/container/goodbox_dispenser_a.png");
+    private final ResourceLocation OBSIDIAN_CONTAINER_RESOURCE_B = new ResourceLocation(Utils.MOD_ID, "textures/gui/container/goodbox_dispenser_b.png");
     private final int textureWidth = 176;
     private final int textureHeight = 166;
     private BlockState state;
@@ -26,26 +28,21 @@ public class GoodsBoxContainerScreen extends ContainerScreen<GoodsBaxContainer> 
     public GoodsBoxContainerScreen(GoodsBaxContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
         state= screenContainer.getBlockState();
-        //screenContainer.
-//        int i = 222;
-//        int j = 114;
-//        this.inventoryRows = container.getNumRows();
-//        this.ySize = 114 + this.inventoryRows * 18;
-//        this.playerInventoryTitleY = this.ySize - 94;
         this.xSize = textureWidth;
         this.ySize = textureHeight;
     }
 
     @Override
     protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int x, int y) {
-        //drawCenteredString(matrixStack, this.font, Integer.toString(this.getContainer().getIntArray().get(0)), 82, 20, 0xeb0505);
+
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int x, int y) {
         this.renderBackground(matrixStack);
-        state.get(TYPE)
-        this.minecraft.getTextureManager().bindTexture(OBSIDIAN_CONTAINER_RESOURCE_A);
+        SlabType slabType = state.get(BlockStateProperties.SLAB_TYPE);
+        if (slabType==SlabType.DOUBLE)  this.minecraft.getTextureManager().bindTexture(OBSIDIAN_CONTAINER_RESOURCE_B);
+        else this.minecraft.getTextureManager().bindTexture(OBSIDIAN_CONTAINER_RESOURCE_A);
         int i = (this.width - this.xSize)/2;
         int j = (this.height - this.ySize)/2;
         //Screen
--- src/main/java/com/aster/justbuildmykeep/entity/BoxContainerTileEntity.java
+++ src/main/java/com/aster/justbuildmykeep/entity/BoxContainerTileEntity.java
@@ -4,6 +4,8 @@ import com.aster.justbuildmykeep.Utils;
 import com.aster.justbuildmykeep.container.GoodsBaxContainer;
 import com.aster.justbuildmykeep.container.ObsidianFirstContainerItemNumber;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.TNTBlock;
+import net.minecraft.entity.item.TNTEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Inventory;
@@ -14,6 +16,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.SlabType;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.LockableLootTileEntity;
 import net.minecraft.util.Direction;
@@ -26,16 +30,17 @@ import javax.annotation.Nullable;
 
 public class BoxContainerTileEntity extends LockableLootTileEntity implements ITickableTileEntity, INamedContainerProvider {
     private BlockState state;
-    private NonNullList<ItemStack> chestContents = NonNullList.withSize(6, ItemStack.EMPTY);
-    private Inventory inventory = new Inventory(6);
+    private NonNullList<ItemStack> chestContents = NonNullList.withSize(12, ItemStack.EMPTY);
+    private Inventory inventory = new Inventory(12);
     private ObsidianFirstContainerItemNumber itemNumber = new ObsidianFirstContainerItemNumber();
-    private int[] chestContentsDirection = new int[6];
-
+    private int[] chestContentsDirection = new int[12];
+    private int boxSize=6;
 
     public BoxContainerTileEntity(BlockState state) {
         super(TileEntityTypeRegistry.BOX_CONTAINER_TILEENTITY.get());
         this.state=state;
-
+        if (state.get(BlockStateProperties.SLAB_TYPE)==SlabType.DOUBLE) this.boxSize=12;
+        else this.boxSize=6;
     }
     public BoxContainerTileEntity() {
         super(TileEntityTypeRegistry.BOX_CONTAINER_TILEENTITY.get());
@@ -80,7 +85,7 @@ public class BoxContainerTileEntity extends LockableLootTileEntity implements IT
 
     @Override
     public int getSizeInventory() {
-        return 6;
+        return boxSize;
     }
 
     @Override
@@ -103,12 +108,12 @@ public class BoxContainerTileEntity extends LockableLootTileEntity implements IT
     @Override
     public void read(BlockState state, CompoundNBT nbt) {
         super.read(state, nbt);
-        this.chestContents = NonNullList.withSize(6, ItemStack.EMPTY);
+        this.chestContents = NonNullList.withSize(boxSize, ItemStack.EMPTY);
         if (!this.checkLootAndRead(nbt)) {
             CompoundNBT contents = nbt.getCompound("contents");
             ItemStackHelper.loadAllItems(contents, this.chestContents);
             this.chestContentsDirection= nbt.getIntArray("directions");
-            for(int i=0;i<6;i++){
+            for(int i=0;i<boxSize;i++){
                 inventory.setInventorySlotContents(i,chestContents.get(i));
             }
         }
@@ -126,14 +131,14 @@ public class BoxContainerTileEntity extends LockableLootTileEntity implements IT
         return compound;
     }
     public NonNullList<ItemStack> getItemList(){
-        NonNullList<ItemStack> list=NonNullList.withSize(6, ItemStack.EMPTY);
-        for(int i=0;i<6;i++){
+        NonNullList<ItemStack> list=NonNullList.withSize(boxSize, ItemStack.EMPTY);
+        for(int i=0;i<boxSize;i++){
             list.set(i,inventory.getStackInSlot(i));
         }
         return list;
     }
     public void setList(){
-        for(int i=0;i<6;i++){
+        for(int i=0;i<boxSize;i++){
             chestContents.set(i,inventory.getStackInSlot(i));
         }
     }
